e3462a89f6b5c92388eb0c4478530e2241f15453
Michael Morin <mmorin5@gmail.com>
2022-12-09 11:25:09 -0800

    Fix incorrect kill pin

--- MAGI_Software/Tasks/PetriDish/waveforms_controller.py
+++ MAGI_Software/Tasks/PetriDish/waveforms_controller.py
@@ -9,7 +9,7 @@ class WFC:
     def __init__(
         self,
         input_pins = {"UP":0, "DOWN":1, "LEFT":2, "RIGHT":3},
-        output_pins = {"CONC":8, "ENERGY":9, "ACCEL_UP":10, "ACCEL_DOWN":11, "ACCEL_LEFT":12, "ACCEL_RIGHT":13, "KILL":0},
+        output_pins = {"CONC":8, "ENERGY":9, "ACCEL_UP":10, "ACCEL_DOWN":11, "ACCEL_LEFT":12, "ACCEL_RIGHT":13, "KILL":14},
         duty_cycle = 50, #%
         max_frequency = 1000 #hz
         ):
