79969fd40fd235785e01b7198c3fa201d10a4194
Michael Morin <mmorin5@gmail.com>
2022-12-09 14:18:33 -0800

    Formatting improvements

--- MAGI_Software/Tasks/PetriDish/waveforms_controller.py
+++ MAGI_Software/Tasks/PetriDish/waveforms_controller.py
@@ -9,13 +9,24 @@ class WFC:
     """
     Connects to the Analog Discovery 2 and allows controlling an agent
     from a PNN and returns feedback for the PNN.
-    
+
     WFC(
-        input_pins = {"UP":0, "DOWN":1, "LEFT":2, "RIGHT":3},
-        output_pins = {"CONC":8, "ENERGY":9, "ACCEL_UP":10, "ACCEL_DOWN":11, "ACCEL_LEFT":12, "ACCEL_RIGHT":13, "KILL":14},
+        input_pins = {"UP":0,
+            "DOWN":1,
+            "LEFT":2,
+            "RIGHT":3
+            },
+        output_pins = {"CONC":8,
+            "ENERGY":9,
+            "ACCEL_UP":10,
+            "ACCEL_DOWN":11,
+            "ACCEL_LEFT":12,
+            "ACCEL_RIGHT":13,
+            "KILL":14},
+
         duty_cycle = 50, #%
         max_frequency = 1000 #hz
-        ) 
+        )
         """
     def __init__(
         self,
@@ -30,8 +41,18 @@ class WFC:
         self.duty_cycle = duty_cycle
         self.max_frequency = max_frequency
 
-        self.input_state = {"UP":0, "DOWN":0, "LEFT":0, "RIGHT":0}
-        self.output_state = {"CONC":0, "ENERGY":0, "ACCEL_UP":0, "ACCEL_DOWN":0, "ACCEL_LEFT":0, "ACCEL_RIGHT":0, "KILL":0}
+        self.input_state = {"UP":0,
+            "DOWN":0,
+            "LEFT":0,
+            "RIGHT":0
+            }
+        self.output_state = {"CONC":0,
+            "ENERGY":0,
+            "ACCEL_UP":0,
+            "ACCEL_DOWN":0,
+            "ACCEL_LEFT":0,
+            "ACCEL_RIGHT":0,
+            "KILL":0}
 
         # Connect to the AD2. Failure currently unhandled.
         self.data = device.open()
@@ -52,13 +73,21 @@ class WFC:
         for output_name, output_value in self.output_state.items():
             channel = self.output_pins[output_name]
             if output_value > 1:
-                print("Error: Output value " + output_name + "is " + str(output_value) + " which is > 1")
+                print("Error: Output value " +
+                 output_name + "is " +
+                 str(output_value) + " which is > 1"
+                 )
             elif output_value == 0:
                 pattern.disable(self.data, channel)
             else:
                 pattern.enable(self.data, channel)
-                pattern.generate(self.data, channel,function=pattern.function.pulse, frequency=self.max_frequency*output_value, duty_cycle=self.duty_cycle)
-                
+                pattern.generate(self.data,
+                                channel,
+                                function=pattern.function.pulse,
+                                frequency=self.max_frequency*output_value,
+                                duty_cycle=self.duty_cycle
+                                )
+
     def update(self):
         """ Run by agents to do I/O through AD2"""
         self.get_inputs()
